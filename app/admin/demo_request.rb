27bdaeef99c6da9fd79305286b2d48a3b02ae738
wonderer007 <alihaider907@gmail.com>
2025-09-10 21:47:52 +0500

    add demo request to the active admin

---
+++ app/admin/demo_request.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register DemoRequest do
+  index do
+    selectable_column
+    id_column
+    column :first_name
+    column :last_name
+    column :email
+    column :restaurant_name
+    column :google_map_url
+    column :created_at
+    actions
+  end
+end

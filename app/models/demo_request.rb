940d950b4becb69d22458d64e5f5cd81bfd0d793
wonderer007 <alihaider907@gmail.com>
2025-09-12 19:08:56 +0500

    remove validation for google map link

--- app/models/demo_request.rb
+++ app/models/demo_request.rb
@@ -3,7 +3,7 @@ class DemoRequest < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :restaurant_name, presence: true, length: { maximum: 100 }
-  validates :google_map_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(['http', 'https']) }
+  # validates :google_map_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(['http', 'https']) }
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "first_name", "google_map_url", "id", "id_value", "last_name", "restaurant_name", "updated_at"]

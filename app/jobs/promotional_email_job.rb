9b57dc0965be9f2f0a5a132415bdd0e4a55976a3
wonderer007 <alihaider907@gmail.com>
2025-09-13 07:22:03 +0500

    cron job to proces emails automatically

--- app/jobs/promotional_email_job.rb
+++ app/jobs/promotional_email_job.rb
@@ -1,8 +1,11 @@
 class PromotionalEmailJob < ApplicationJob
   queue_as :default
 
-  def perform
-    Outreach::Email.where(email_sent_at: nil).find_each do |email|
+  # If no email_ids are provided, process the first 90 unsent emails.
+  def perform(email_ids = nil)
+    scope = Outreach::Email.where(email_sent_at: nil)
+    scope = scope.where(id: email_ids) if email_ids.present?
+    scope.limit(90).find_each do |email|
       send_email(email)
       sleep get_random_delay.seconds
     end
@@ -16,6 +19,8 @@ class PromotionalEmailJob < ApplicationJob
   end
 
   def get_random_delay
+    return 0 if Rails.env.development?
+
     rand(5..20)
   end
 end

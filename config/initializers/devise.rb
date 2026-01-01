ba8c146122227e23c1ceef886ac423fb13d3fb8c
wonderer007 <alihaider907@gmail.com>
2026-01-01 06:11:54 +0500

    fix env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :google_oauth2,
-                  ENV.fetch('GOOGLE_CLIENT_ID'),
-                  ENV.fetch('GOOGLE_CLIENT_SECRET'),
+                  ENV['GOOGLE_CLIENT_ID'],
+                  ENV['GOOGLE_CLIENT_SECRET'],
                   {
                     scope: 'email,profile',
                     prompt: 'select_account',

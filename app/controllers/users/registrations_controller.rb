2ab174bdd41e08ed25d70e749ffdfc882d3c821a
wonderer007 <alihaider907@gmail.com>
2026-01-02 01:35:29 +0500

    fix minor things

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,12 +4,51 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   helper_method :current_business
 
+  # Override create to handle business creation like OAuth flow
+  def create
+    build_resource(sign_up_params)
+
+    ActiveRecord::Base.transaction do
+      # Create business first, then associate (same as OAuth flow)
+      business = Business.create!(business_type: :google_place, status: :created)
+      resource.business = business
+      resource.save!
+    end
+
+    yield resource if block_given?
+
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  rescue ActiveRecord::RecordInvalid
+    clean_up_passwords resource
+    set_minimum_password_length
+    respond_with resource
+  end
+
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       :first_name, :last_name
     ])
+  end
+
+  def after_sign_up_path_for(resource)
+    # Redirect to onboarding flow (same as OAuth)
+    onboarding_path
   end
 
   private

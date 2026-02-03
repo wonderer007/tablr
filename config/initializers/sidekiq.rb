5dce96d9813facee874f43a5c12fe0f346acc98c
wonderer007 <alihaider907@gmail.com>
2026-02-04 02:33:14 +0500

    reload sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,9 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(user), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USER"])) &
   Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
 end
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq::Scheduler.reload_schedule!
+  end
+end

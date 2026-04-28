9bbab3a9a4c9da27774ad90611ea0751855bf8e1
wonderer007 <alihaider907@gmail.com>
2026-04-29 00:27:18 +0200

    schdule automatic marketing emails

---
+++ app/jobs/marketing/schedule_outreach_job.rb
@@ -0,0 +1,31 @@
+module Marketing
+  class ScheduleOutreachJob < ApplicationJob
+    queue_as :default
+
+    def perform
+      company = next_company_for_outreach
+      return unless company
+
+      Rails.logger.info("[Marketing::ScheduleOutreachJob] Enqueuing CompleteProcessingJob for company_id=#{company.id}")
+      Marketing::CompleteProcessingJob.perform_later(company.id)
+    end
+
+    private
+
+    def next_company_for_outreach
+      ready_ids = Marketing::Company
+                    .ready_for_outreach
+                    .where(flagged: false)
+                    .pluck(:id)
+
+      return if ready_ids.empty?
+
+      Marketing::Company
+        .where(id: ready_ids)
+        .left_joins(:marketing_contacts)
+        .group("marketing_companies.id")
+        .order(Arel.sql("COUNT(marketing_contacts.id) ASC"))
+        .first
+    end
+  end
+end
